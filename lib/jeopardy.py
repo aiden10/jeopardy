7f67abbf2bc0e614492eea79738f1ff165dd3c50
aiden10 <aiden.ten30@gmail.com>
2024-05-15 21:02:14 -0400

    made stateful

--- lib/jeopardy.py
+++ lib/jeopardy.py
@@ -1,73 +1,31 @@
-# import json
-# import os 
-# import subprocess
+"""
+Want:
+Random question from random game, its category, and its answer
+2 random numbers needed, one, the question number and two, the game number 
 
-# fh = open("jeopardy.json", encoding="utf8")
-# data = json.load(fh)
-# first = data["1"]["jeopardy"]
-# for question in first:
-#     spaces = " " * int(question["x"])
-#     print(question["cat"])
-#     print((question["q"], question["val"]), spaces, end="")
+Random game number: (1, 9013)
+Random question number: (0, len(data[game_number]['jeopardy']))
 
+Extracting entire game:
+    data[game_number]['jeopardy']
 
+Extracting additional data:
+    data[question_number]['a'] # Answer
+    data[question_number]['cat'] # Category
+    data[question_number]['val'] # Value
 
+"""
 
+import json
+import random 
 
+fh = open(r"C:\Users\aiden\Documents\Jeopardy\jeopardy\lib\jeopardy.json", encoding="utf8")
+data = json.load(fh)
+first = data["1"]["jeopardy"]
+for question in first:
+    print(question)
+    # print(question['a'])
+    # spaces = " " * int(question["x"])
+    # print(question["cat"])
+    # print((question["q"], question["val"]), spaces, end="")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import time
-
-driver = webdriver.Firefox()  # or webdriver.Firefox()
-
-# URL of the first page
-url = "https://www.patreon.com/posts/i-became-tyrant-85247078"
-f = open("Tyrant of a Defense Game.txt", "a", encoding='utf-8')
-chapter_num = 1
-while True:
-    text = "\n\n\n Chapter " + str(chapter_num) + '\n'
-    # Navigate to the current page
-    driver.get(url)
-    time.sleep(2)  # Optional: Wait for the page to load, adjust as needed
-
-    # Get the HTML content of the page
-    page_source = driver.page_source
-
-    # Parse the HTML content with BeautifulSoup
-    soup = BeautifulSoup(page_source, 'html.parser')
-
-    # Extract text from <p> tags
-    paragraphs = soup.find('div', {'class': 'sc-1ye87qi-0 bCBphS'}).find_all('p')
-
-    for paragraph in paragraphs:
-        text += paragraph.get_text() + "\n"
-
-    f.write(text)
-    chapter_num += 1
-    # Find the link to the next page
-    next_page_link = soup.find('a', string='Next')
-
-    if next_page_link:
-        # Get the URL of the next page
-        url = next_page_link['href']
-    else:
-        # If there is no next page link, break the loop
-        break
-
-# Close the browser
-driver.quit()
-f.close()
--- lib/main.dart
+++ lib/main.dart
@@ -62,34 +62,111 @@ Flutter specifics it's hard to know exactly what is and isn't known at compile t
 Widget sizes/units: can use different widgets to achieve desired effect (FractionallySizedBox or Expanded), use 'hardcoded' numbers instead of percentages.
 Can use MediaQuery.of(context).size.height or width to get height or width of device and scale appropiately 
 
+Stateful Widgets: 
+Stateful widgets extend the StatefulWidget class and must also be paired with a generic 'State' class. This state class also extends another 
+class, which will be the type of the widget <T>. Types are specified like in Java with the greater than less than signs. Then your stateful widget
+will have a createState method to create an instance of the State class. Then you can call setState to update its state. 
+I think the general idea is pretty straightforward at least. You have a state widget class and it might have some parameters,
+you initialize with some default value for the parameters and then can call setState with new arguments to update the state. The hard part would be
+understanding how Flutter handles the process exactly and the syntax for it. 
+Example:
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){ // build method here is automatically called by Flutter I think
+    return MaterialApp(home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(); // I don't quite get this line
+}
+
+class _MyHomePageState extends State<MyHomePage> { // class which represents a State of an instance of MyHomePage
+  int _counter = 0; // private variable(?) not sure what underscores indicate
+
+  void _incrementCounter() { // method which increments the counter
+    setState(() {
+      _counter++;
+    });
+  }
+
+To do:
+- Load JSON
+- Properly display random questions and their category
+- Make skip button load a random question
+- Add buttons to designate incorrect and correct guesses when text is submitted
+- Indicators for correct and incorrect (score?)
+- Stats
 */
 import 'package:flutter/material.dart';
 import 'variables.dart';
+import 'dart:math'; // For generating random numbers
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // Widget representing the app as a whole
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context) { // how it gets built
+    return const MaterialApp( // returns a MaterialApp widget which contains the MyHomePage widget
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget { // stateful MyHomePage widget 
   const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(); // every StatefulWidget must implement the createState method and have a State which then gets updated
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentCategory = "Geography";
+  String _currentQuestion = "(Jimmy of the Clue Crew presents by a display monitor.) The border between the Greek regions of Macedonia and Thessaly is the location of this legendary home of the gods";
+
+  final List<Map<String, String>> _questions = [
+    {
+      "category": "Geography",
+      "question": "(Jimmy of the Clue Crew presents by a display monitor.) The border between the Greek regions of Macedonia and Thessaly is the location of this legendary home of the gods"
+    },
+    {
+      "category": "Science",
+      "question": "What is the powerhouse of the cell?"
+    },
+    {
+      "category": "History",
+      "question": "Who was the first president of the United States?"
+    },
+  ];
+
+  void _skipQuestion() {
+    final random = Random();
+    final newQuestion = _questions[random.nextInt(_questions.length)];
+    setState(() {
+      _currentCategory = newQuestion["category"]!;
+      _currentQuestion = newQuestion["question"]!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     Variables.init(context); // Load class with constant variables for use
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(121, 150, 132, 132),
+      backgroundColor: const Color.fromARGB(121, 184, 171, 171),
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
@@ -99,15 +176,15 @@ class MyHomePage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: Variables.screenHeight * 0.1),
+                SizedBox(height: Variables.screenHeight * 0.15),
                 Container(
                   color: const Color.fromARGB(255, 240, 238, 233),
                   width: Variables.screenWidth * 0.8,
                   padding: EdgeInsets.all(Variables.screenHeight * 0.02),
-                  child: const Center(
+                  child: Center(
                     child: Text(
-                      "Geography",
-                      style: TextStyle(
+                      _currentCategory,
+                      style: const TextStyle(
                         fontSize: 32,
                         fontWeight: FontWeight.bold,
                       ),
@@ -119,10 +196,10 @@ class MyHomePage extends StatelessWidget {
                   padding: EdgeInsets.symmetric(
                     horizontal: Variables.screenWidth * 0.05,
                   ),
-                  child: const Text(
-                    "(Jimmy of the Clue Crew presents by a display monitor.) The border between the Greek regions of Macedonia and Thessaly is the location of this legendary home of the gods",
+                  child: Text(
+                    _currentQuestion,
                     textAlign: TextAlign.center,
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 18,
                       fontStyle: FontStyle.italic,
                       color: Colors.white,
@@ -135,15 +212,15 @@ class MyHomePage extends StatelessWidget {
                   child: TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      border: OutlineInputBorder(),
                       fillColor: Colors.white,
+                      border: OutlineInputBorder(),
                       hintText: 'Type here...',
                     ),
                   ),
                 ),
                 SizedBox(height: Variables.screenHeight * 0.1),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _skipQuestion,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(177, 204, 23, 23),
                     shape: const RoundedRectangleBorder(
